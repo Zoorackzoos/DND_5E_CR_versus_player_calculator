d099ba356970a6a214f1c9e8e18fc75fdabbbaa9
Zoorackzoos <duncancholmes@gmail.com>
2026-05-15 00:10:00 -0500

    URL, try.

--- src/dungeons/intro/intro_The_Turtle_Hideout_calvin.py
+++ src/dungeons/intro/intro_The_Turtle_Hideout_calvin.py
@@ -13,7 +13,19 @@ def intro_the_Turtle_Hideout_calvin(tab_amount="\t"):
     player_levels = [3,3,3,3]
     all_humanoids = get_row_from_dict_on_param_type_and_string(dict_in_question=monsters_all_stats_dict,param_type="Type",string="humanoid",tab_amount=tab_amount)
 
-    print_2d_list(list=all_humanoids,tab_amount=tab_amount)
+    """
+    how to sort lists of dicts
+        don't ask bruh
+    the key contains the key it sorts by
+    reverse is if you want top to bottum or bottum to top
+    """
+    all_humanoids.sort(
+        key=lambda row: float(row["CR"]),
+        reverse=True
+    )
+
+    print_2d_list(list_in_question=all_humanoids, tab_amount=tab_amount)
+
 
 if __name__ == "__main__":
     intro_the_Turtle_Hideout_calvin()
--- src/universal_functions/display/print_2d_list.py
+++ src/universal_functions/display/print_2d_list.py
@@ -1,4 +1,49 @@
-def print_2d_list(list, tab_amount="\t"):
-    print(tab_amount,"print_2d_list")
-    for row in list:
-        print(tab_amount+"\t",row)
+def print_2d_list(list_in_question, tab_amount="\t"):
+
+    if len(list_in_question) == 0:
+        print(tab_amount, "empty list")
+        return
+
+    headers = list_in_question[0].keys()
+
+    # Calculate max width of each column
+    column_widths = {}
+
+    for header in headers:
+        max_width = len(header)
+
+        for row in list_in_question:
+            value_length = len(str(row[header]))
+
+            if value_length > max_width:
+                max_width = value_length
+
+        column_widths[header] = max_width
+
+    # Print headers
+    header_line = ""
+
+    for header in headers:
+        width = column_widths[header]
+        header_line += f"{header:<{width}} | "
+
+    print(tab_amount + header_line)
+
+    # Print separator
+    separator = ""
+
+    for header in headers:
+        separator += "-" * column_widths[header] + "-+-"
+
+    print(tab_amount + separator)
+
+    # Print rows
+    for row in list_in_question:
+
+        line = ""
+
+        for header in headers:
+            width = column_widths[header]
+            line += f"{str(row[header]):<{width}} | "
+
+        print(tab_amount + line)
