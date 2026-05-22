5982d190f34e79a75e8d0741614968de1203176e
Zoorackzoos <duncancholmes@gmail.com>
2026-05-22 18:06:11 -0500

    need 2

---
+++ computer_minigames/hthq_door_minigame.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+
+def hthq_door_minigame(tab_amount=""):
+    time.sleep(1)
+    print("initializing Evil Ninja OS...")
+    time.sleep(1)
+    os.system("cls")
+    #TODO: get rid of the goofy ahh \0
+    print("\nfuck")
+
+if __name__ == "__main__":
+    hthq_door_minigame()
