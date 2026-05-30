08fbd826e84cc0e9cdbfc1c077543e573c679592
Zoorackzoos <duncancholmes@gmail.com>
2026-05-30 18:25:35 -0500

    done

--- src/dungeons/1_phase_1/Michelangelo/hthq.py
+++ src/dungeons/1_phase_1/Michelangelo/hthq.py
@@ -153,6 +153,47 @@ def get_roadkill_rodney_cr(tab_amount="\t"):
         }
     plug_monster_var_values_into_get_cr_from_monster(monster_var=roadkill_rodney_monster_dict, tab_amount=tab_amount)
 
+def get_metalhead_cr(tab_amount="\t"):
+    metalhead_dice_dict = \
+        {
+            8: 2,
+            "constant": 6
+        }
+    metalhead_machine_gun_chest_ability_dice_dict = \
+        {
+            4: 2,
+            "constant": 2
+        }
+    metalhead_monster_xp = \
+        {
+            "hp": 60,
+            "ac": 14,
+            "average_damage": get_average_damage(dice_dict=metalhead_dice_dict, tab_amount=tab_amount),
+            "attack_modifier": 4,
+            "has_legendary_action": False,
+            "has_flight": False,
+            "resistance_count": 0,
+            "immunity_count": 0,
+            "weakness_count": 0,
+            "save_dc": 13,
+            "is_spellcaster": False,
+            "regeneration_per_round": 0,
+            "multiattack_count": 0,
+            "ability_count": 2,
+            "recharge_damage": 0,
+            "limited_use_damage": 0,
+            "bonus_action_damage": get_average_damage(dice_dict=metalhead_machine_gun_chest_ability_dice_dict,tab_amount=tab_amount),
+            "legendary_action_damage": 0,
+            "ability_cr_weight": 1,
+            "str_modifier": 2,
+            "dex_modifier": 2,
+            "con_modifier": 0,
+            "int_modifier": -1,
+            "wis_modifier": -1,
+            "cha_modifier": -1
+        }
+    plug_monster_var_values_into_get_cr_from_monster(monster_var=metalhead_monster_xp, tab_amount=tab_amount)
+
 #murder all human traffickers. every single one.
 def hthq(tab_amount="\t"):
     print(tab_amount,"hthq")
@@ -262,11 +303,28 @@ def hthq(tab_amount="\t"):
     roadkill_rodney_xp = get_xp_from_single_enemy_cr(cr=roadkill_rodney_cr,
                                                      tab_amount=tab_amount)
 
-    lvl_3_gene_banks_monster_xps = [roadkill_rodney_xp, roadkill_rodney_xp, roadkill_rodney_xp,
+    lvl_3_test_tube_monster_xps = [roadkill_rodney_xp, roadkill_rodney_xp, roadkill_rodney_xp,
                                     roadkill_rodney_xp, roadkill_rodney_xp, roadkill_rodney_xp]
-    lvl_3_gene_banks_encounter_difficulty = get_encounter_difficulty(player_levels=player_levels,
-                                                                     monster_xp_values=lvl_3_gene_banks_monster_xps,
+    lvl_3_test_tube_encounter_difficulty = get_encounter_difficulty(player_levels=player_levels,
+                                                                     monster_xp_values=lvl_3_test_tube_monster_xps,
                                                                      tab_amount=tab_amount)
+
+    metalhead_cr = get_cr_from_precise_monster_search(param_type=SpreadsheetKeysEnums.NAME.value,
+                                                      string="Metalhead",
+                                                      path_to_csv_file=hthq_path_to_csv_file,
+                                                      tab_amount=tab_amount)
+    metalhead_xp = get_xp_from_single_enemy_cr(cr=metalhead_cr,tab_amount=tab_amount)
+
+    lvl_3_gene_banks_monster_xps = \
+        [
+            yellow_ninja_xp, yellow_ninja_xp,
+            green_ninja_xp, green_ninja_xp, green_ninja_xp,
+            green_ninja_xp, green_ninja_xp, green_ninja_xp,
+            #metalhead_xp #metalhead does appear once they interact with the door.... so don't include him.
+        ]
+    lvl_3_gene_banks_monster_encounter_difficulty = get_encounter_difficulty(player_levels=player_levels,
+                                                                             monster_xp_values=lvl_3_gene_banks_monster_xps,
+                                                                             tab_amount=tab_amount)
 
     print(tab_amount,"calculations complete :-3")
     print(tab_amount+'\t',"dict_in_question=lvl_1_encounter_difficulty")
@@ -278,8 +336,11 @@ def hthq(tab_amount="\t"):
     print(tab_amount + '\t', "dict_in_question=lvl_2_personal_and_mainframe_encounter_difficulty")
     print_dictionary_nicely(dict_in_question=lvl_2_personal_and_mainframe_encounter_difficulty,
                             tab_amount=tab_amount + "\t\t")
-    print(tab_amount + '\t', "dict_in_question=lvl_3_gene_banks_encounter_difficulty")
-    print_dictionary_nicely(dict_in_question=lvl_3_gene_banks_encounter_difficulty,
+    print(tab_amount + '\t', "dict_in_question=lvl_3_test_tube_encounter_difficulty")
+    print_dictionary_nicely(dict_in_question=lvl_3_test_tube_encounter_difficulty,
+                            tab_amount=tab_amount + "\t\t")
+    print(tab_amount + '\t', "dict_in_question=lvl_3_gene_banks_monster_encounter_difficulty")
+    print_dictionary_nicely(dict_in_question=lvl_3_gene_banks_monster_encounter_difficulty,
                             tab_amount=tab_amount + "\t\t")
 
 if __name__ == "__main__":
@@ -289,3 +350,4 @@ if __name__ == "__main__":
     #get_rahzar_cr(tab_amount=tab_amount)
     #get_cyborg_cr(tab_amount=tab_amount)
     #get_roadkill_rodney_cr(tab_amount=tab_amount)
+    #get_metalhead_cr(tab_amount=tab_amount)
