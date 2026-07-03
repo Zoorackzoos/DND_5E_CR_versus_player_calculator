77e8d1ff795ca0c39ecf45ee622983479d7e2032
Zoorackzoos <thrakofvirasco@gmail.com>
2026-07-03 02:02:42 -0500

    the_hero.py : interp 1/2

--- src/monster_dict_files/phase_1_monster_spreadsheet_updater_file.py
+++ src/monster_dict_files/phase_1_monster_spreadsheet_updater_file.py
@@ -28,146 +28,8 @@ from src.universal_functions.spreadsheet_stuff.spreadsheet_updaters.update_homeb
 from src.universal_functions.stat_block_interpreter.interpret_markdown_stat_block import \
     interpret_markdown_stat_block_into_python_file
 
-path_to_monster_spreadsheet_file = \
-    "../../sheets/monsters_all_stats_homebrew/monsters_all_stats_homebrew.csv"
-
-def nuclear_interpret_phase_1_michelangelo_monster_files(tab_amount="\t"):
-    """
-    #TODO: if you want to update the values in this funciton. update the strings. becuase they're hardcoded
-
-    :param tab_amount:
-    :return:
-    """
-    print(tab_amount,"interpret_phase_1_michelangelo_monster_files")
-    tab_amount += "\t"
-
-    phase_1_michelangelo_boss_markdown_and_python_paths = \
-    [
-        [
-            "monster_dict_files/michelangelo_bosses/Rahzar_the_evil_puppy_monster.md",
-            "monster_dict_files/michelangelo_bosses/Rahzar_the_evil_puppy_monster.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_bosses/Tokka_the_evil_blue_turtle.md",
-            "monster_dict_files/michelangelo_bosses/Tokka_the_evil_blue_turtle.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_bosses/Metalhead.md",
-            "monster_dict_files/michelangelo_bosses/Metalhead.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_bosses/Bebop_the_Warthog.md",
-            "monster_dict_files/michelangelo_bosses/Bebop_the_Warthog.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_bosses/Rocksteady_the_Rhinovirus.md",
-            "monster_dict_files/michelangelo_bosses/Rocksteady_the_Rhinovirus.py"
-        ],
-    ]
-
-    phase_1_michelangelo_phase_1_foot_clan_ninjas_markdown_and_python_paths = \
-    [
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_blue_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_blue_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_green_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_green_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_orange_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_orange_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_pink_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_pink_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_purple_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_purple_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_white_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_white_foot_clan_ninja.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_yellow_foot_clan_ninja.md",
-            "monster_dict_files/michelangelo_phase_1_foot_clan_ninjas/phase_1_yellow_foot_clan_ninja.py"
-        ]
-    ]
-
-    phase_1_michelangelo_rouge_enemies_markdown_and_python_paths = \
-    [
-        [
-            "monster_dict_files/michelangelo_rouge_monsters/Roadkill_Rodney.md",
-            "monster_dict_files/michelangelo_rouge_monsters/Roadkill_Rodney.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_rouge_monsters/evil_foot_clan_ninja_cyborg.md",
-            "monster_dict_files/michelangelo_rouge_monsters/evil_foot_clan_ninja_cyborg.py"
-        ],
-        [
-            "monster_dict_files/michelangelo_rouge_monsters/evil_foot_clan_ninja_cyborg.md",
-            "monster_dict_files/michelangelo_rouge_monsters/evil_foot_clan_ninja_cyborg.py"
-        ]
-    ]
-
-    print(tab_amount, "phase_1_michelangelo_boss_markdown_and_python_paths")
-    for markdown_and_python_file_list in phase_1_michelangelo_boss_markdown_and_python_paths:
-        interpret_markdown_stat_block_into_python_file(
-            path_to_markdown_file=markdown_and_python_file_list[0],
-            path_to_python_file=markdown_and_python_file_list[1],
-            tab_amount=tab_amount+"\t"
-        )
-
-    print(tab_amount,"phase_1_michelangelo_phase_1_foot_clan_ninjas_markdown_and_python_paths")
-    for markdown_and_python_file_list in phase_1_michelangelo_phase_1_foot_clan_ninjas_markdown_and_python_paths:
-        interpret_markdown_stat_block_into_python_file(
-            path_to_markdown_file=markdown_and_python_file_list[0],
-            path_to_python_file=markdown_and_python_file_list[1],
-            tab_amount=tab_amount+"\t"
-        )
-
-    print(tab_amount,"phase_1_michelangelo_rouge_enemies_markdown_and_python_paths")
-    for markdown_and_python_file_list in phase_1_michelangelo_rouge_enemies_markdown_and_python_paths:
-        interpret_markdown_stat_block_into_python_file(
-            path_to_markdown_file=markdown_and_python_file_list[0],
-            path_to_python_file=markdown_and_python_file_list[1],
-            tab_amount=tab_amount+"\t"
-        )
-
-def nuclear_update_phase_1_michelangelo_monsters_onto_spreadsheet(tab_amount="\t"):
-    print(tab_amount,"update_phase_1_michelangelo_monsters_onto_spreadsheet")
-    tab_amount += "\t"
-
-    phase_1_monster_and_boss_dicts = \
-    [
-        phase_1_blue_foot_clan_ninja_monster_dict,
-        phase_1_green_foot_clan_ninja_monster_dict,
-        phase_1_orange_foot_clan_ninja_monster_dict,
-        phase_1_purple_foot_clan_ninja_monster_dict,
-        phase_1_white_foot_clan_ninja_monster_dict,
-        phase_1_yellow_foot_clan_ninja_monster_dict,
-        phase_1_pink_foot_clan_ninja_monster_dict,
-        rahzar_the_evil_puppy_monster_monster_dict,
-        tokka_the_evil_blue_turtle_monster_dict,
-        roadkill_rodney_monster_dict,
-        evil_foot_clan_ninja_cyborg_monster_dict,
-        metalhead_monster_dict
-    ]
-
-    for monster_dict in phase_1_monster_and_boss_dicts:
-        update_homebrew_monster_spreadsheet(
-            monster_dict=monster_dict,
-            path_to_csv_file=path_to_monster_spreadsheet_file,
-            tab_amount=tab_amount,
-        )
-
-    convert_csv_file_into_tsv_file(
-        path_to_csv_file=path_to_monster_spreadsheet_file,
-        tab_amount=tab_amount
-    )
+path_to_monsters_csv_spreadsheet_file = \
+    "../../sheets/encounter_feedback/encounter_feedback.csv"
 
 def interp_forest_rouge_monsters(tab_amount="\t"):
     forest_rouge_monster_markdown_and_python_paths = \
@@ -189,8 +51,10 @@ def interp_forest_rouge_monsters(tab_amount="\t"):
             tab_amount=tab_amount+"\t"
         )
 
-def interp_calculus_monsters(tab_amount="\t"):
+def update_forest_rouge_monsters(tab_amount="\t"):
+    pass
 
+def interp_calculus_monsters(tab_amount="\t"):
     calculus_monster_markdown_and_python_paths = \
         [
             [
@@ -222,5 +86,10 @@ def interp_calculus_monsters(tab_amount="\t"):
             tab_amount=tab_amount + "\t"
         )
 
+def update_calculus_monsters(tab_amount="\t"):
+    pass
+
 if __name__ == "__main__":
     tab_amount = "\t"
+    interp_forest_rouge_monsters(tab_amount=tab_amount)
+    interp_calculus_monsters(tab_amount=tab_amount)
