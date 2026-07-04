0f33361a48d235b91253619575aa2f3bd0e276e6
Zoorackzoos <thrakofvirasco@gmail.com>
2026-07-04 03:54:34 -0500

    must add names to encounters. bedtime. when will i learn?

--- src/universal_functions/get_encounter_difficulty/get_encounter_difficulty_from_cr_values.py
+++ src/universal_functions/get_encounter_difficulty/get_encounter_difficulty_from_cr_values.py
@@ -8,6 +8,17 @@ def get_encounter_difficulty_from_cr_values(player_levels,
                                             monster_cr_values,
                                             encounter_name="NOT_NAMED",
                                             tab_amount="\t"):
+    """
+    main end goal of this repo.
+    The actual function takes in xp values, so this is a helper function that takes Cr values instead
+    #TODO: make the encounter name paramater auto-fill with the variable that contains the result of this function. consult a clanker because that sounds impossible.
+
+    :param player_levels: list of integers
+    :param monster_cr_values: list of doubles
+    :param encounter_name: string
+    :param tab_amount: variations of "\t". like "\t\t" or "\t\t\t"
+    :return: encounter_dict
+    """
     print(tab_amount, "get_encounter_difficulty_from_cr_values")
     tab_amount += "\t"
 
